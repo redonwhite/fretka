c4448a07f11b1b22a2e1f3e65ab33f8c2f7a1a8c
Paweł Pohl <pawp@netcompany.com>
2021-03-20 22:10:53 +0100

    One more time... with snowpack
    initial commit
    
    initial commit

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-import { notes, modesOfMajor, makeRootedScale, } from "./domain/notes.js";
-notes.basicNotesArray
-    .map(note => makeRootedScale(modesOfMajor.ionian, note))
-    .forEach(mode => console.log(`${mode.root.id}: ${Object.keys(mode.notes).join(", ")}`));
-console.log("hajime");
